77acd7e073f2f175115e874b6c471114f2676291
dohung <dohuyhung12@gmail.com>
2022-06-19 10:34:59 +0700

    extract wrapper

---
+++ client/src/assets/Wrapper/AvatarWrapper.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  img {
+    width: 2.5rem !important;
+    height: auto;
+    border-radius: 50%;
+    border: 1px solid var(--secondary);
+    max-width: fit-content;
+  }
+`;
+
+export default Wrapper;
--- client/src/components/Avatar.js
+++ client/src/components/Avatar.js
@@ -1,16 +1,4 @@
-import React from 'react';
-import styled from 'styled-components';
-
-const Wrapper = styled.div`
-  img {
-    width: 2.5rem !important;
-    height: auto;
-    border-radius: 50%;
-    border: 1px solid var(--secondary);
-    max-width: fit-content;
-  }
-`;
-
+import Wrapper from '../assets/Wrapper/AvatarWrapper';
 const Avatar = ({ src }) => {
   return (
     <Wrapper>
